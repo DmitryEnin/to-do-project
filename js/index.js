7485a655c0fff6b3935d9d7c8702a1952b48a39f
waranya-sun <waranya.sun@gmail.com>
2022-06-29 13:30:41 +1000

    To match the requirements

--- js/index.js
+++ js/index.js
@@ -28,8 +28,8 @@ class Validation {
     return input === '' ? false : true;
   }
 
-  static isBetween(inputLength, min, max) {
-    return inputLength > min && inputLength <= max ? true : false;
+  static isMoreThan(inputLength, min) {
+    return inputLength >= min ? true : false;
   }
 
   static isFormattedDate(input) {
@@ -40,16 +40,18 @@ class Validation {
 
   static addCheckName() {
     let valid = false;
-    const min = 3;
-    const max = 10;
+    const min = 8;
     const name = addInputName.value.trim();
 
     if (!Validation.isRequired(name)) {
-      Validation.showError(addInputName, 'Name cannot be blank.');
-    } else if (!Validation.isBetween(name.length, min, max)) {
       Validation.showError(
         addInputName,
-        `Name must be between ${min} and ${max} characters.`
+        `Name cannot be blank and at least ${min} characters long.`
+      );
+    } else if (!Validation.isMoreThan(name.length, min)) {
+      Validation.showError(
+        addInputName,
+        `Name must be more than ${min} characters.`
       );
     } else {
       Validation.showSuccess(addInputName);
@@ -61,25 +63,47 @@ class Validation {
 
   static addCheckDescription() {
     let valid = false;
+    const min = 15;
     const description = addInputDescription.value.trim();
-    if (Validation.isRequired(description)) {
+
+    if (!Validation.isRequired(description)) {
+      Validation.showError(
+        addInputDescription,
+        `Description cannot be blank and at least ${min} characters long.`
+      );
+    } else if (!Validation.isMoreThan(description.length, min)) {
+      Validation.showError(
+        addInputDescription,
+        `Description must be more than ${min} characters.`
+      );
+    } else {
       Validation.showSuccess(addInputDescription);
       valid = true;
-    } else {
-      Validation.showError(addInputDescription, 'Description cannot be blank.');
     }
+
     return valid;
   }
 
   static addCheckAssignedTo() {
     let valid = false;
+    const min = 8;
     const assignedTo = addInputAssignedTo.value.trim();
-    if (Validation.isRequired(assignedTo)) {
+
+    if (!Validation.isRequired(assignedTo)) {
+      Validation.showError(
+        addInputAssignedTo,
+        `AssignedTo cannot be blank and at least ${min} characters long.`
+      );
+    } else if (!Validation.isMoreThan(assignedTo.length, min)) {
+      Validation.showError(
+        addInputAssignedTo,
+        `AssignedTo must be more than ${min} characters.`
+      );
+    } else {
       Validation.showSuccess(addInputAssignedTo);
       valid = true;
-    } else {
-      Validation.showError(addInputAssignedTo, 'Assigned to cannot be blank.');
     }
+
     return valid;
   }
 
@@ -87,7 +111,10 @@ class Validation {
     let valid = false;
     const dueDate = addInputDueDate.value.trim();
     if (!Validation.isRequired(dueDate)) {
-      Validation.showError(addInputDueDate, 'Due date cannot be blank.');
+      Validation.showError(
+        addInputDueDate,
+        'Due date cannot be blank and cannot select past date.'
+      );
     } else if (!Validation.isFormattedDate(dueDate)) {
       Validation.showError(
         addInputDueDate,
@@ -97,6 +124,7 @@ class Validation {
       Validation.showSuccess(addInputDueDate);
       valid = true;
     }
+
     return valid;
   }
 
@@ -112,18 +140,45 @@ class Validation {
     return valid;
   }
 
+  static validateAddTaskForm() {
+    let isNameValid = Validation.addCheckName(),
+      isDescriptionValid = Validation.addCheckDescription(),
+      isAssignedToValid = Validation.addCheckAssignedTo(),
+      isDueDateValid = Validation.addCheckDueDate(),
+      isStatusValid = Validation.addCheckStatus();
+
+    let isFormValid =
+      isNameValid &&
+      isDescriptionValid &&
+      isAssignedToValid &&
+      isDueDateValid &&
+      isStatusValid;
+
+    if (isFormValid) {
+      // To fix the Modal from not closing after click Submit button (from e.preventDefault());
+      addSubmitBtn.setAttribute('data-bs-dismiss', 'modal');
+      addSubmitBtn.click();
+      // To remove this attribute from the modal (IIFE)
+      (() => {
+        addSubmitBtn.setAttribute('data-bs-dismiss', '');
+      })();
+    }
+  }
+
   static editCheckName() {
     let valid = false;
-    const min = 3;
-    const max = 10;
+    const min = 8;
     const name = editInputName.value.trim();
 
     if (!Validation.isRequired(name)) {
-      Validation.showError(editInputName, 'Name cannot be blank.');
-    } else if (!Validation.isBetween(name.length, min, max)) {
       Validation.showError(
         editInputName,
-        `Name must be between ${min} and ${max} characters.`
+        `Name cannot be blank and at least ${min} characters long.`
+      );
+    } else if (!Validation.isMoreThan(name.length, min)) {
+      Validation.showError(
+        editInputName,
+        `Name must be more than ${min} characters.`
       );
     } else {
       Validation.showSuccess(editInputName);
@@ -135,28 +190,47 @@ class Validation {
 
   static editCheckDescription() {
     let valid = false;
+    const min = 15;
     const description = editInputDescription.value.trim();
-    if (Validation.isRequired(description)) {
-      Validation.showSuccess(editInputDescription);
-      valid = true;
-    } else {
+
+    if (!Validation.isRequired(description)) {
       Validation.showError(
         editInputDescription,
-        'Description cannot be blank.'
+        `Description cannot be blank and at least ${min} characters long.`
       );
+    } else if (!Validation.isMoreThan(description.length, min)) {
+      Validation.showError(
+        editInputDescription,
+        `Description must be more than ${min} characters.`
+      );
+    } else {
+      Validation.showSuccess(editInputDescription);
+      valid = true;
     }
+
     return valid;
   }
 
   static editCheckAssignedTo() {
     let valid = false;
+    const min = 8;
     const assignedTo = editInputAssignedTo.value.trim();
-    if (Validation.isRequired(assignedTo)) {
+
+    if (!Validation.isRequired(assignedTo)) {
+      Validation.showError(
+        editInputAssignedTo,
+        `AssignedTo cannot be blank and at least ${min} characters long.`
+      );
+    } else if (!Validation.isMoreThan(assignedTo.length, min)) {
+      Validation.showError(
+        editInputAssignedTo,
+        `AssignedTo must be more than ${min} characters.`
+      );
+    } else {
       Validation.showSuccess(editInputAssignedTo);
       valid = true;
-    } else {
-      Validation.showError(editInputAssignedTo, 'Assigned to cannot be blank.');
     }
+
     return valid;
   }
 
@@ -164,7 +238,10 @@ class Validation {
     let valid = false;
     const dueDate = editInputDueDate.value.trim();
     if (!Validation.isRequired(dueDate)) {
-      Validation.showError(editInputDueDate, 'Due date cannot be blank.');
+      Validation.showError(
+        editInputDueDate,
+        'Due date cannot be blank and cannot select past date.'
+      );
     } else if (!Validation.isFormattedDate(dueDate)) {
       Validation.showError(
         editInputDueDate,
@@ -187,6 +264,31 @@ class Validation {
       Validation.showError(editInputStatus, 'Status cannot be blank.');
     }
     return valid;
+  }
+
+  static validateEditTaskForm() {
+    let isNameValid = Validation.editCheckName(),
+      isDescriptionValid = Validation.editCheckDescription(),
+      isAssignedToValid = Validation.editCheckAssignedTo(),
+      isDueDateValid = Validation.editCheckDueDate(),
+      isStatusValid = Validation.editCheckStatus();
+
+    let isFormValid =
+      isNameValid &&
+      isDescriptionValid &&
+      isAssignedToValid &&
+      isDueDateValid &&
+      isStatusValid;
+
+    if (isFormValid) {
+      // To fix the Modal from not closing after click Submit button (from e.preventDefault());
+      editSaveBtn.setAttribute('data-bs-dismiss', 'modal');
+      editSaveBtn.click();
+      // To remove this attribute from the modal (IIFE)
+      (() => {
+        editSaveBtn.setAttribute('data-bs-dismiss', '');
+      })();
+    }
   }
 
   static showError(input, message) {
@@ -217,50 +319,56 @@ class Utility {
     const date = new Date();
     const today =
       date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear();
-    const dateTextNode = document.createTextNode(today);
-    return dateTextNode;
+    // const dateTextNode = document.createTextNode(today);
+
+    const myDateSpan = document.createElement('span');
+    myDateSpan.textContent = today;
+    return myDateSpan;
   }
 
   static showTime() {
     const date = new Date();
-    const timeTextNode = document.createTextNode(date.toLocaleTimeString());
-    return timeTextNode;
+    // const timeTextNode = document.createTextNode(date.toLocaleTimeString());
+
+    const myTimeSpan = document.createElement('span');
+    myTimeSpan.textContent = date.toLocaleTimeString();
+    return myTimeSpan;
   }
 }
 
 //////////////////////////////////////////////////// All event section /////////////////////////////////////////////////////
 
+//////////////// Show Date and Time when page load ////////////////
+showDate.appendChild(Utility.showDate());
+showTime.appendChild(Utility.showTime());
+
+//////////////// All about Adding Form Events /////////////////////
 // Event: Adding Submit Form
 formAddTask.addEventListener('submit', e => {
   e.preventDefault();
-  // console.log(addInputName.value);
-  // console.log(addInputDescription.value);
-  // console.log(addInputAssignedTo.value);
-  // console.log(addInputDueDate.value);
-  // console.log(addInputStatus.value);
+  Validation.validateAddTaskForm();
+  // let isNameValid = Validation.addCheckName(),
+  //   isDescriptionValid = Validation.addCheckDescription(),
+  //   isAssignedToValid = Validation.addCheckAssignedTo(),
+  //   isDueDateValid = Validation.addCheckDueDate(),
+  //   isStatusValid = Validation.addCheckStatus();
 
-  let isNameValid = Validation.addCheckName(),
-    isDescriptionValid = Validation.addCheckDescription(),
-    isAssignedToValid = Validation.addCheckAssignedTo(),
-    isDueDateValid = Validation.addCheckDueDate(),
-    isStatusValid = Validation.addCheckStatus();
+  // let isFormValid =
+  //   isNameValid &&
+  //   isDescriptionValid &&
+  //   isAssignedToValid &&
+  //   isDueDateValid &&
+  //   isStatusValid;
 
-  let isFormValid =
-    isNameValid &&
-    isDescriptionValid &&
-    isAssignedToValid &&
-    isDueDateValid &&
-    isStatusValid;
-
-  if (isFormValid) {
-    // To fix the Modal from not closing after click Submit button (from e.preventDefault());
-    addSubmitBtn.setAttribute('data-bs-dismiss', 'modal');
-    addSubmitBtn.click();
-    // To remove this attribute from the modal (IIFE)
-    (() => {
-      addSubmitBtn.setAttribute('data-bs-dismiss', '');
-    })();
-  }
+  // if (isFormValid) {
+  //   // To fix the Modal from not closing after click Submit button (from e.preventDefault());
+  //   addSubmitBtn.setAttribute('data-bs-dismiss', 'modal');
+  //   addSubmitBtn.click();
+  //   // To remove this attribute from the modal (IIFE)
+  //   (() => {
+  //     addSubmitBtn.setAttribute('data-bs-dismiss', '');
+  //   })();
+  // }
 });
 
 // Event: Click on Reset Button on Add Modal
@@ -269,38 +377,14 @@ addResetBtn.addEventListener('click', Validation.resetAddFormFields);
 // Event: Click on X Button on Add Modal
 addCloseModal.addEventListener('click', Validation.resetAddFormFields);
 
+//////////////// All about Editing Form Events ////////////////////
 // Event: Save Edited Task
 editSaveBtn.addEventListener('click', e => {
   e.preventDefault();
-  let isNameValid = Validation.editCheckName(),
-    isDescriptionValid = Validation.editCheckDescription(),
-    isAssignedToValid = Validation.editCheckAssignedTo(),
-    isDueDateValid = Validation.editCheckDueDate(),
-    isStatusValid = Validation.editCheckStatus();
-
-  let isFormValid =
-    isNameValid &&
-    isDescriptionValid &&
-    isAssignedToValid &&
-    isDueDateValid &&
-    isStatusValid;
-
-  if (isFormValid) {
-    // To fix the Modal from not closing after click Submit button (from e.preventDefault());
-    editSaveBtn.setAttribute('data-bs-dismiss', 'modal');
-    editSaveBtn.click();
-    // To remove this attribute from the modal (IIFE)
-    (() => {
-      editSaveBtn.setAttribute('data-bs-dismiss', '');
-    })();
-  }
+  Validation.validateEditTaskForm();
 });
 
 // Event: Reset to Prev Data of a Task
 editResetBtn.addEventListener('click', () => {
   document.getElementById('formEditTask').reset();
 });
-
-// Show Date and Time when page load
-showDate.appendChild(Utility.showDate());
-showTime.appendChild(Utility.showTime());
