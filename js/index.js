e0ac22bca3da83621e3ac00d2dfc32bc7a11015b
waranya-sun <waranya.sun@gmail.com>
2022-06-30 13:29:57 +1000

    Create functions

--- js/index.js
+++ js/index.js
@@ -23,6 +23,187 @@ let editBtn = document.getElementsByClassName('editBtn');
 const showDate = document.querySelector('#showDate');
 const showTime = document.querySelector('#showTime');
 
+const todoColumn = document.querySelector('#todoColumn');
+const inProgressColumn = document.querySelector('#inProgressColumn');
+const reviewColumn = document.querySelector('#reviewColumn');
+const doneColumn = document.querySelector('#doneColumn');
+
+// Create the id
+let myID = 0;
+let taskArray = [
+  {
+    id: '1',
+    name: 'meet team',
+    description: 'at the office at 10.30AM',
+    assignedTo: 'john wayne',
+    dueDate: '08-06-2022',
+    status: 'todo',
+  },
+  {
+    id: '2',
+    name: 'pay the gas bill',
+    description: 'pay for 3 months',
+    assignedTo: 'Sarah Brown',
+    dueDate: '09-06-2022',
+    status: 'in progress',
+  },
+  {
+    id: '3',
+    name: 'food shopping',
+    description: 'need to check all lists',
+    assignedTo: 'jane jackson',
+    dueDate: '24-06-2022',
+    status: 'review',
+  },
+  {
+    id: '4',
+    name: 'go to the bank',
+    description: 'check all the accounts',
+    assignedTo: 'mark timber',
+    dueDate: '05-06-2022',
+    status: 'done',
+  },
+  {
+    id: '5',
+    name: 'pick up from school',
+    description: 'have to arrive at 5.15PM',
+    assignedTo: 'mark timber',
+    dueDate: '07-06-2022',
+    status: 'done',
+  },
+];
+
+class Task {
+  constructor(id, name, description, assignedTo, dueDate, status) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.assignedTo = assignedTo;
+    this.dueDate = dueDate;
+    this.status = status;
+  }
+}
+
+class TaskManager {
+  static getAllTasks() {
+    const tasks = taskArray;
+    console.log(tasks);
+    tasks.forEach(task => TaskManager.render(task));
+  }
+
+  static getTasksWithStatus(status) {
+    const allTasksWithSameStatus = taskArray.filter(
+      task => task.status === status
+    );
+    console.log(allTasksWithSameStatus);
+  }
+
+  static addTask(task) {
+    taskArray.push(task);
+    console.log({ taskArray });
+  }
+
+  static createTaskHTML(task) {
+    TaskManager.render(task);
+  }
+
+  static render(task) {
+    const divElement = document.createElement('div');
+    divElement.classList.add('col-12', 'text-start', 'mb-2');
+    divElement.innerHTML = `<div class='card border-secondary'>
+    <div class='card-body boxShadow'>
+      <h5 class='card-title fs-4 pb-3 text-capitalize'>${task.name}</h5>
+      <p class='card-text pb-3 firstLetter'>${task.description}</p>
+      <p class='card-text text-capitalize'><span class='fw-bold pe-3'>Assigned to :</span>${
+        task.assignedTo
+      }</p>
+      <p class='card-text'><span class='fw-bold pe-3'>Due Date :</span>${
+        task.dueDate
+      }</p>
+      <p class='card-text pb-3 text-capitalize'><span class='fw-bold pe-3'>Status :</span><span class=${Utility.checkColor(
+        task.status
+      )}>${task.status}</span></p>
+      <div class='col d-flex justify-content-between align-items-center'>
+        <div style='display:none'>${task.id}</div>
+        <button class='btn border-dark delete' type='submit'>Delete</button>
+        ${
+          task.status === 'done'
+            ? `<span></span>`
+            : `<button type='button' class='btn border border-dark taskStatus'>Mark as Done</button>`
+        }
+        <button class='btn border border-dark edit editRemoveFocus' type='submit' data-bs-toggle='modal' data-bs-target='#staticEditBackdrop'>Edit</button>
+      </div>
+    </div>
+</div>`;
+
+    let statusColumn = TaskManager.addToStatusColumn(task.status);
+    statusColumn.appendChild(divElement);
+  }
+
+  static addToStatusColumn(status) {
+    switch (status) {
+      case 'todo':
+        return todoColumn;
+      case 'in progress':
+        return inProgressColumn;
+      case 'review':
+        return reviewColumn;
+      case 'done':
+        return doneColumn;
+    }
+  }
+
+  static editTask(e) {
+    const taskId =
+      e.target.previousElementSibling.previousElementSibling
+        .previousElementSibling.textContent;
+    const editedTask = taskArray.filter(task => task.id === taskId);
+    editInputId.textContent = taskId;
+    editInputName.value = editedTask[0].name;
+    editInputDescription.value = editedTask[0].description;
+    editInputAssignedTo.value = editedTask[0].assignedTo;
+    editInputDueDate.value = editedTask[0].dueDate;
+    editInputStatus.value = editedTask[0].status;
+
+    editInputNamePrev = editedTask[0].name;
+    editInputDescriptionPrev = editedTask[0].description;
+    editInputAssignedToPrev = editedTask[0].assignedTo;
+    editInputDueDatePrev = editedTask[0].dueDate;
+    editInputStatusPrev = editedTask[0].status;
+  }
+
+  static saveEditedTask(e) {
+    e.preventDefault();
+    const taskId =
+      e.target.previousElementSibling.previousElementSibling.textContent;
+    taskArray.forEach(task => {
+      if (task.id === taskId) {
+        task.name = editInputName.value;
+        task.description = editInputDescription.value;
+        task.assignedTo = editInputAssignedTo.value;
+        task.dueDate = editInputDueDate.value;
+        task.status = editInputStatus.value;
+      }
+    });
+
+    console.log({ taskArray });
+    editSaveBtn.setAttribute('data-bs-dismiss', 'modal');
+    editSaveBtn.click();
+    (() => {
+      editSaveBtn.setAttribute('data-bs-dismiss', '');
+    })();
+  }
+
+  static resetEditedTask(e) {
+    e.preventDefault();
+    editInputName.value = editInputNamePrev;
+    editInputDescription.value = editInputDescriptionPrev;
+    editInputAssignedTo.value = editInputAssignedToPrev;
+    editInputDueDate.value = editInputDueDatePrev;
+    editInputStatus.value = editInputStatusPrev;
+  }
+}
+
 class Validation {
   static isRequired(input) {
     return input === '' ? false : true;
@@ -141,6 +322,7 @@ class Validation {
   }
 
   static validateAddTaskForm() {
+    let valid = false;
     let isNameValid = Validation.addCheckName(),
       isDescriptionValid = Validation.addCheckDescription(),
       isAssignedToValid = Validation.addCheckAssignedTo(),
@@ -162,7 +344,10 @@ class Validation {
       (() => {
         addSubmitBtn.setAttribute('data-bs-dismiss', '');
       })();
+      valid = true;
     }
+
+    return valid;
   }
 
   static editCheckName() {
@@ -315,6 +500,19 @@ class Validation {
 
 // Utility Class: Provides Utility Methods
 class Utility {
+  static create_UUID() {
+    var dt = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(
+      /[xy]/g,
+      function (c) {
+        var r = (dt + Math.random() * 16) % 16 | 0;
+        dt = Math.floor(dt / 16);
+        return (c == 'x' ? r : (r & 0x3) | 0x8).toString(16);
+      }
+    );
+    return uuid;
+  }
+
   static showDate() {
     const date = new Date();
     const today =
@@ -334,41 +532,64 @@ class Utility {
     myTimeSpan.textContent = date.toLocaleTimeString();
     return myTimeSpan;
   }
+
+  static checkColor(status) {
+    switch (status) {
+      case 'todo':
+        return 'red';
+      case 'done':
+        return 'green';
+      case 'in progress':
+        return 'orange';
+      case 'review':
+        return 'blue';
+    }
+  }
 }
 
 //////////////////////////////////////////////////// All event section /////////////////////////////////////////////////////
 
-//////////////// Show Date and Time when page load ////////////////
+// Show Date and Time when page load
 showDate.appendChild(Utility.showDate());
 showTime.appendChild(Utility.showTime());
 
-//////////////// All about Adding Form Events /////////////////////
+// Event: Display TodoList on the screen
+document.addEventListener('DOMContentLoaded', TaskManager.getAllTasks);
+
+// All about Adding Form Events
 // Event: Adding Submit Form
 formAddTask.addEventListener('submit', e => {
   e.preventDefault();
-  Validation.validateAddTaskForm();
-  // let isNameValid = Validation.addCheckName(),
-  //   isDescriptionValid = Validation.addCheckDescription(),
-  //   isAssignedToValid = Validation.addCheckAssignedTo(),
-  //   isDueDateValid = Validation.addCheckDueDate(),
-  //   isStatusValid = Validation.addCheckStatus();
+  const isAddFormValid = Validation.validateAddTaskForm();
+  if (isAddFormValid) {
+    // const id = myID++;
+    const id = Utility.create_UUID();
+    const name = addInputName.value;
+    const description = addInputDescription.value;
+    const assignedTo = addInputAssignedTo.value;
+    const dueDate = addInputDueDate.value;
+    const status = addInputStatus.value;
 
-  // let isFormValid =
-  //   isNameValid &&
-  //   isDescriptionValid &&
-  //   isAssignedToValid &&
-  //   isDueDateValid &&
-  //   isStatusValid;
+    // For JSON object
+    const taskJSON = {
+      id: id,
+      name: name,
+      description: description,
+      assignedTo: assignedTo,
+      dueDate: dueDate,
+      status: status,
+    };
 
-  // if (isFormValid) {
-  //   // To fix the Modal from not closing after click Submit button (from e.preventDefault());
-  //   addSubmitBtn.setAttribute('data-bs-dismiss', 'modal');
-  //   addSubmitBtn.click();
-  //   // To remove this attribute from the modal (IIFE)
-  //   (() => {
-  //     addSubmitBtn.setAttribute('data-bs-dismiss', '');
-  //   })();
-  // }
+    //For Task Object
+    // const task = new Task(id, name, description, assignedTo, dueDate, status);
+
+    //Add a task object to taskArray
+    TaskManager.addTask(task);
+    TaskManager.createTaskHTML(task);
+
+    //Reset the add form
+    Validation.resetAddFormFields();
+  }
 });
 
 // Event: Click on Reset Button on Add Modal
@@ -387,4 +608,15 @@ editSaveBtn.addEventListener('click', e => {
 // Event: Reset to Prev Data of a Task
 editResetBtn.addEventListener('click', () => {
   document.getElementById('formEditTask').reset();
+});
+
+// Event: Listen to event of Delete OR Edit OR Mark as Done
+showTaskList.addEventListener('click', e => {
+  if (e.target.classList.contains('delete')) {
+    return null;
+  } else if (e.target.classList.contains('taskStatus')) {
+    return null;
+  } else if (e.target.classList.contains('edit')) {
+    TaskManager.editTask(e);
+  }
 });
